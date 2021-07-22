acb96ba28c53a6df7c89abe97527ea1c46ff05eb
e.barkalov <barkalov125@mail.ru>
2021-07-22 15:22:08 +0300

    продолжаю indexeddb taskServices

--- todo/src/services/TaskServices/TaskServices.ts
+++ todo/src/services/TaskServices/TaskServices.ts
@@ -5,101 +5,142 @@ type Task = {
 };
 
 class TaskServices {
-  db: IDBDatabase;
-  request: IDBOpenDBRequest;
-  storeName = "taskStore";
+  private db?: IDBDatabase;
+  private request: IDBOpenDBRequest;
+  private storeName: string;
+  private dbName: string;
 
-  constructor() {
-    this.request = window.indexedDB.open("taskdb");
+  constructor(dbName: string, storeName: string) {
+    this.dbName = dbName;
+    this.storeName = storeName;
 
-    this.request.onupgradeneeded = (event: any) => {
-      this.db = event.target.result;
-      this.db.createObjectStore(this.storeName, { keyPath: "id" , autoIncrement: true});
+    this.request = indexedDB.open(this.dbName);
+
+    this.request.onsuccess = () => {
+      this.db = this.request.result;
+    };
+
+    this.request.onupgradeneeded = () => {
+      this.db = this.request.result;
+      this.db.createObjectStore(this.storeName, {
+        keyPath: "id",
+        autoIncrement: true,
+      });
     };
     this.request.onerror = (error) => {
-      console.log("Initialize error", {error});
+      console.log("Initialize error", { error });
     };
   }
 
-  initialize(){
-    
-  }
-  
+  initialize() {}
 
-  getObjectStore(){
-    console.log(this.db);
-    const transaction = this.db.transaction(this.storeName, "readwrite");
+  createStore() {
+    if (!this.db) {
+      return;
+    }
+  }
+
+  getObjectStore() {
+    if (!this.db) {
+      return;
+    }
+    const transaction = this.db.transaction([this.storeName], "readwrite");
     transaction.onerror = () => {
       console.log("GetObjectStore error", transaction.error);
     };
     return transaction.objectStore(this.storeName);
-  };
+  }
 
   getAllTasks = () => {
-    const objectStore = this.getObjectStore();
-
-    const cursor = objectStore.openCursor();
-    const tasksList = [];
-
-    cursor.onsuccess = (event: any) => {
-      const innerCursor = event.target.result;
-      if (innerCursor) {
-        tasksList.push(innerCursor.value);
-        innerCursor.continue();
+    return new Promise((resolve, reject) => {
+      const objectStore = this.getObjectStore();
+      if (!objectStore) {
+        return reject("store or db is undefined");
       }
-    };
+      const cursor = objectStore.openCursor();
+      const items: Task[] = [];
+
+      cursor.onsuccess = (event: any) => {
+        const innerCursor = event.target.result;
+        if (innerCursor) {
+          items.push(innerCursor.value);
+          innerCursor.continue();
+        } else {
+          resolve(items);
+        }
+      };
+    });
   };
 
   insert(task: Task) {
-    const objectStore = this.getObjectStore();
-    const addRequest = objectStore.add(task);
-    addRequest.onsuccess = () => {
-      //...
-    };
-    addRequest.onerror = (error) => {
-      console.log("Insert error",{error});
-    };
+    return new Promise((resolve, reject) => {
+      const objectStore = this.getObjectStore();
+      if (!objectStore) {
+        return reject("store or db is undefined");
+      }
+      const addRequest = objectStore.add(task);
+      addRequest.onsuccess = () => {
+        const allItems = this.getAllTasks();
+        resolve(allItems);
+      };
+      addRequest.onerror = (error) => {
+        reject(error);
+      };
+    });
   }
 
   delete(id: number) {
-    const objectStore = this.getObjectStore();
-    const delRequest = objectStore.delete(id);
-    delRequest.onsuccess = () => {
-      //...
-    };
-    delRequest.onerror = (error) => {
-      console.log("Delete error",{error});
-    };
+    return new Promise((resolve, reject) => {
+      const objectStore = this.getObjectStore();
+      if (!objectStore) {
+        return reject("store or db is undefined");
+      }
+      const delRequest = objectStore.delete(id);
+      delRequest.onsuccess = () => {
+        const allItems = this.getAllTasks();
+        resolve(allItems);
+      };
+      delRequest.onerror = (error) => {
+        reject(error);
+      };
+    });
   }
 
   update(id: number, newTask: Task) {
-    const objectStore = this.getObjectStore();
-    const keyRange = IDBKeyRange.only(id);
-    const cursor = objectStore.openCursor(keyRange);
-
-    cursor.onsuccess = () => {
-      const cursorWithValue = cursor?.result;
-      const value  = cursorWithValue?.value;
-
-      const updateTo = { ...value, ...newTask };
-
-      if(cursorWithValue === null){
-        return;
+    return new Promise((resolve, reject) => {
+      const objectStore = this.getObjectStore();
+      if (!objectStore) {
+        return reject("store or db is undefined");
       }
+      const keyRange = IDBKeyRange.only(id);
+      const cursor = objectStore.openCursor(keyRange);
 
-      const update = cursorWithValue.update(updateTo);
+      cursor.onsuccess = () => {
+        const cursorWithValue = cursor?.result;
+        const value = cursorWithValue?.value;
 
-      update.onsuccess = () => {
-        //...
+        const updateTo = { ...value, ...newTask };
+
+        if (cursorWithValue === null) {
+          return reject("cursorWithValue is undefined");
+        }
+
+        const update = cursorWithValue.update(updateTo);
+
+        update.onsuccess = () => {
+          const allItems = this.getAllTasks();
+          resolve(allItems);
+        };
+        update.onerror = (error) => {
+          reject(error);
+        };
       };
-      update.onerror = (error) => {
-        console.log({ error });
+      cursor.onerror = (error) => {
+        console.log("Check error", { error });
       };
-    };
-    cursor.onerror = (error) => {
-      console.log("Check error", { error });
-    };
+    });
   }
 }
 
+export const taskServices = new TaskServices("tasksdb", "tasksStore");
 export default TaskServices;
